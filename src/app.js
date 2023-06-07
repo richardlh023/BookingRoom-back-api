6d7f4be2a97f5513b3b4a14ec8d49dd110a284f8
chaninwit <120734528+chaninwit@users.noreply.github.com>
2023-06-07 11:58:13 +0700

    init project

---
+++ src/app.js
@@ -0,0 +1,22 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+
+const notFoundMiddleware = require("./middlewares/not-found");
+const errorMiddleware = require("./middlewares/error");
+
+const app = express();
+
+if (process.env.NODE_ENV === "develop") {
+  app.use(morgan("dev"));
+}
+
+app.use(cors());
+app.use(express.json());
+
+app.use(notFoundMiddleware);
+app.use(errorMiddleware);
+
+const port = process.env.PORT || 8000;
+app.listen(port, () => console.log("server running on port " + port));
