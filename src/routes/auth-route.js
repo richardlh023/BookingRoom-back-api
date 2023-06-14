d17a4d9f98c637bf15d98631a09c3dbf137f8b12
chaninwit <120734528+chaninwit@users.noreply.github.com>
2023-06-14 16:16:37 +0700

    sent

--- src/routes/auth-route.js
+++ src/routes/auth-route.js
@@ -6,6 +6,5 @@ const router = express.Router();
 
 router.post("/register", authController.register);
 router.post("/login", authController.login);
-router.get("/profile", authController.getProfile);
 
 module.exports = router;
